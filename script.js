17bf79d22c7dd1d0846b380db8d2a0ff85a52512
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2025-01-07 16:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+document.getElementById("sub_btn").addEventListener("click", () => {
+    const age = Number(document.getElementById("age").value);
+    const name = document.getElementById("name").value;
+
+    if (!name) {
+        alert("Please enter your name.");
+        return;
+    }
+
+    if (isNaN(age) || age <= 0) {
+        alert("Please enter a valid age.");
+        return;
+    }
+
+    verify(age, name)
+        .then((data) => {
+            alert(data);
+        })
+        .catch((err) => {
+            alert(err);
+        });
+});
+
+function verify(age, name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age > 18) {
+                resolve("Welcome, "+name+". You can vote.");
+            } else {
+                reject("Oh sorry "+ name+". You aren't old enough.");
+            }
+        }, 4000);
+    });
+}
